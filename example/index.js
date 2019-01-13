e051eb745434af97f6d0eb5c3ec67965f7edeb6f
evila <ijoec123@gmail.com>
2019-01-13 20:21:46 +0800

    Rename test -> example

---
+++ example/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { routes } from '../'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes
+})
+
+new Vue({
+  el: '#app',
+  router,
+  render(h) {
+    return h('router-view')
+  }
+})

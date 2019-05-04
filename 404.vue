463e1005d6f026bbf430ef6c0db16a12aaeb0667
evillt <ijoec123@gmail.com>
2019-05-04 17:41:29 +0800

    perf(404): convert to functional component
    re #6

--- 404.vue
+++ 404.vue
@@ -1,57 +1,32 @@
-<!--
-  Borrow from vuepress
-  https://github.com/vuejs/vuepress
-  vuepress/lib/default-theme/NotFound.vue
--->
-<template>
-  <div class="Page-404">
-    <h1>404</h1>
-    <blockquote>{{ getMsg() }}</blockquote>
-    <router-link to="/">Take me home!</router-link>
-  </div>
-</template>
-
 <script>
-const msgs = [
-  `There's nothing here.`,
-  `How did we get here?`,
-  `That's a Four-Oh-Four.`,
-  `Looks like we've got some broken links.`
-]
-
 export default {
-  methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
-    }
+  functional: true,
+  render(h) {
+    return h(
+      'div',
+      {
+        style: {
+          font: '16px/1.6 Roboto, Helvetica, Neue, san-serif',
+          margin: '2rem'
+        }
+      },
+      [
+        h('h1', { style: { margin: '0 0 20px 0' } }, ['Page Not Found :(']),
+        h(
+          'router-link',
+          {
+            props: {
+              to: '/'
+            },
+            style: {
+              'text-decoration': 'none',
+              color: '#41b882'
+            }
+          },
+          ['← Back to HomePage']
+        )
+      ]
+    )
   }
 }
 </script>
-
-<style scoped>
-blockquote {
-  font-size: 1.2rem;
-  color: #999;
-  border-left: .25rem solid #dfe2e5;
-  margin-left: 0;
-  padding-left: 1rem;
-}
-h1 {
-  color: #2c3e50;
-  font-weight: 600;
-  line-height: 1.25;
-  font-size: 2.2rem;
-  margin-top: 4.6rem;
-  margin-bottom: 1rem;
-}
-a {
-  color: #3eaf7c;
-  font-weight: 500;
-  text-decoration: none;
-}
-.Page-404 {
-  padding: 2rem;
-  max-width: 740px;
-  margin: 0 auto;
-}
-</style>

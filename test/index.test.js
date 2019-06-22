f3ccbddb09251034c77af4dcbdd34fe9a5e7a3ba
evillt <ijoec123@gmail.com>
2019-06-22 23:24:29 +0800

    test: update snapshots

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,7 @@
-import path from 'path'
 import test from 'ava'
-import {
-  renderRoutes,
-  collectRoutes,
-  renderRoutesMap
-} from '../lib/collect-fs-routes'
-import Plugin from '../lib/plugin'
+import { renderRoutes, collectRoutes } from '../lib/collect-fs-routes'
 
-const dir = path.join(__dirname, 'views')
+const dir = 'test/views'
 const options = {
   dir,
   match: 'vue,js'
@@ -15,17 +9,10 @@ const options = {
 
 test('main', async t => {
   const routes = await collectRoutes(options)
-
-  const routesString = renderRoutes(routes)
-  const routesMap = renderRoutesMap(routes)
+  const routesString = renderRoutes(routes, {
+    componentPrefix: ''
+  })
 
   t.snapshot(JSON.stringify(routes), 'collect routes')
   t.snapshot(routesString, 'render routes')
-  t.snapshot(routesMap, 'render routes map')
-})
-
-test('plugin routes file', async t => {
-  const plugin = new Plugin(options)
-
-  t.snapshot(await plugin.handleRoutesString(), 'plugin routes file')
 })

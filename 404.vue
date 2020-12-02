9016e70fe4610d708b0ee24bc85fd4a5395b086f
evillt <ijoec123@gmail.com>
2020-12-02 14:28:11 +0800

    feat: support vue router next

---
+++ 404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :style="{
+    font: '16px/1.6 Roboto, Helvetica, Neue, san-serif',
+    margin: '2rem'
+  }">
+    <h1 style="margin: 0 0 20px 0">
+      Page Not Found :(
+    </h1>
+    <router-link
+      to="/"
+      style="text-decoration: none; color: #41b882;"
+    >
+      ← Back to HomePage
+    </router-link>
+  </div>
+</template>
